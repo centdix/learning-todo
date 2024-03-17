16151a4b599ad9bf88c6088c73a1351e76a829d2
Farhad <farhadg@the-ifc.com>
2024-03-17 17:41:35 +0100

    add remove button

--- src/script.js
+++ src/script.js
@@ -1,15 +1,38 @@
-const createItem = (itemText) => {
+let list = [];
+
+const deleteItem = (index) => {
+    list.splice(index, 1);
+    renderList(list);
+}
+
+const createDeleteButton = (index) => {
+    const button = document.createElement('button');
+    button.innerHTML = 'remove';
+    button.classList.add('delete-btn');
+    button.addEventListener('click', () => {
+        deleteItem(index);
+    });
+    return button;
+}
+
+const createItem = (itemText, index) => {
     // creer un element li
     const li = document.createElement('li');
 
     // ajouter le texte de l'item
-    li.innerHTML = itemText;
+    li.innerText = itemText;
+
+    // creer un bouton pour supprimer l'item
+    const button = createDeleteButton(index);
+
+    // ajouter le bouton comme un enfant de li
+    li.appendChild(button);
 
     // retourner l'element li
     return li;
 
     // ce qui donnera
-    // <li>{itemText}</li>;
+    // <li>{itemText}<button>remove</button></li>;
 }
 
 const renderList = (list) => {
@@ -19,23 +42,17 @@ const renderList = (list) => {
     // on vide la liste pour re-afficher les items de zero, sinon on aurait des doublons
     listContainer.innerHTML = '';
 
-    list.forEach((itemText) => {
+    list.forEach((itemText, index) => {
         // creer un element li
-        const li = createItem(itemText);
+        const li = createItem(itemText, index);
 
         // l'ajouter comme un enfant de listContainer (qui sera donc le parent)
         listContainer.appendChild(li);
     });
-
-    // pour chaque item dans la liste:
-        // creer un element li
-        // l'ajouter comme un enfant de listContainer (qui sera donc le parent)
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let list = [];
     input.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
             // on recupere le texte de l'input
